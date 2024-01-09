016b3bd7733b606a922711bd6f57f22157eee9f5
Salman <amithasan319@gmail.com>
2024-01-10 01:49:05 +0600

    new

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// vite.config.js
-export default {
-  base: '/your-repo-name/',
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/modern_bank/',
+  plugins: [react()]
+})
